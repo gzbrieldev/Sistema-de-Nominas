8d784c61e1edc0f744d1207318ab7da728f75818
Gabriel Alcequiez <20251062@itla.edu.do>
2026-01-16 17:14:35 -0400

    Añadir funcionalidad de ActualizarUsuario a la capa de presentacion, Program.cs

--- Sistema_Nominas/Presentacion/Program.cs
+++ Sistema_Nominas/Presentacion/Program.cs
@@ -14,8 +14,16 @@ namespace Sistema_Nominas.Presentacion
             int seleccion = 0;
             do
             {
-                decimal ventasBrutas = 0;
-                int tarifaComision = 0;
+                string nombre;
+                string apellidoPaterno;
+                string nss;
+                decimal ventasBrutas;
+                int tarifaComision;
+                decimal sueldoPorHora; 
+                decimal salarioSemanal;
+                int horasTrabajadas;
+
+                Console.WriteLine("###############################");
                 Console.WriteLine("Sistema de Nominas");
 
                 Console.WriteLine("\nMenú");
@@ -23,23 +31,27 @@ namespace Sistema_Nominas.Presentacion
                 Console.WriteLine("2. Actualizar empleados");
                 Console.WriteLine("3. Lista de empleados");
                 Console.WriteLine("4. Generar reporte semanal");
+                Console.WriteLine("5. Limpiar consola");
                 Console.WriteLine("0. Salir");
+                Console.WriteLine("\n###############################");
 
                 Console.Write("\nSeleccione una opción (1-5): ");
                 seleccion = int.Parse(Console.ReadLine());
+
+
 
                 switch (seleccion)
                 {
                     case 1:
                         Console.Write("Ingrese el primer nombre: ");
-                        string nombre = Console.ReadLine();
+                        nombre = Console.ReadLine();
                         Console.Write("Ingrese el apellido paterno: ");
-                        string apellidoPaterno = Console.ReadLine();
+                        apellidoPaterno = Console.ReadLine();
                         Console.Write("Ingrese su NSS (id): ");
-                        string nss = Console.ReadLine();
+                        nss = Console.ReadLine();
 
-                        Console.WriteLine("\n¿Qué tipo de empleado es?\n1. Asalariado\n2. Por Horas\n3. Por Comisión\n4. Asalariado por comisión");
-                        Console.Write("Seleccione el tipo de usuario (1-4): ");
+                        Console.WriteLine("\n¿Qué tipo de empleado es?\n1. Asalariado/Asalariado por comisión\n2. Por Horas\n3. Por Comisión");
+                        Console.Write("Seleccione el tipo de usuario (1-3): ");
                         int seleccionUsuario = int.Parse(Console.ReadLine());
 
                         switch (seleccionUsuario)
@@ -47,7 +59,7 @@ namespace Sistema_Nominas.Presentacion
                             
                             case 1:
                                 Console.Write("Ingrese su salario semanal: ");
-                                decimal salarioSemanal = decimal.Parse(Console.ReadLine());
+                                salarioSemanal = decimal.Parse(Console.ReadLine());
 
                                 Console.Write("¿Es asalariado por comisión? (1. Si / 2. No): ");
                                 int esAPC = int.Parse(Console.ReadLine());
@@ -57,7 +69,7 @@ namespace Sistema_Nominas.Presentacion
                                     Console.Write("Ingrese las ventas brutas: ");
                                     ventasBrutas = decimal.Parse(Console.ReadLine());
 
-                                    Console.Write("Ingrese la tarifa de comisión (1-100)");
+                                    Console.Write("Ingrese la tarifa de comisión (1-100): ");
                                     tarifaComision = int.Parse(Console.ReadLine());
 
                                     servicio.RegistrarEmpleados(new EmpleadoAsalariadoPorComision(nombre, apellidoPaterno, nss, salarioSemanal,ventasBrutas, tarifaComision));
@@ -73,10 +85,10 @@ namespace Sistema_Nominas.Presentacion
                             case 2:
 
                                 Console.Write("Ingrese el sueldo por hora: ");
-                                decimal sueldoPorHora = decimal.Parse(Console.ReadLine());
+                                sueldoPorHora = decimal.Parse(Console.ReadLine());
 
                                 Console.Write("Ingrese las horas trabajadas: ");
-                                int horasTrabajadas = int.Parse(Console.ReadLine());
+                                horasTrabajadas = int.Parse(Console.ReadLine());
 
                                 servicio.RegistrarEmpleados(new EmpleadoPorHoras(nombre, apellidoPaterno, nss, sueldoPorHora, horasTrabajadas));
                                 Console.WriteLine("Empleado por horas agregado correctamente!");
@@ -86,7 +98,7 @@ namespace Sistema_Nominas.Presentacion
                                 Console.Write("Ingrese las ventas brutas: ");
                                 ventasBrutas = decimal.Parse(Console.ReadLine());
 
-                                Console.Write("Ingrese la tarifa de comisión (1-100)");
+                                Console.Write("Ingrese la tarifa de comisión (1-100): ");
                                 tarifaComision = int.Parse(Console.ReadLine());
 
                                 servicio.RegistrarEmpleados(new EmpleadoPorComision(nombre, apellidoPaterno, nss, ventasBrutas, tarifaComision));
@@ -94,6 +106,89 @@ namespace Sistema_Nominas.Presentacion
                                 break;
 
 
+                        }
+                        break;
+
+                    case 2:
+                        Console.Write("Ingrese el NSS del empleado a actualizar: ");
+                        nss = Console.ReadLine();
+
+                        var existente = servicio.BuscarEmpleado(nss);
+
+                        if (existente == null)
+                        {
+                            Console.WriteLine("Empleado no encontrado. No se puede actualizar.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Empleado encontrado: {existente}");
+
+                            Console.Write("Ingrese el nuevo nombre (ENTER SI DESEA DEJAR EL MISMO): ");
+                            nombre = Console.ReadLine();
+
+                            if(nombre == "")
+                            {
+                                nombre = existente.PrimerNombre;
+                            }
+
+                            Console.Write("Ingrese el nuevo apellido (ENTER SI DESEA DEJAR EL MISMO): ");
+                            string apellido = Console.ReadLine();
+
+                            if (apellido == "")
+                            {
+                                apellido = existente.ApellidoPaterno;
+                            }
+
+                            Empleado empActualizado = null;
+
+                            if (existente is EmpleadoAsalariado)
+                            {
+                                Console.Write("Ingrese el nuevo salario semanal: ");
+                                salarioSemanal = decimal.Parse(Console.ReadLine());
+
+                                empActualizado = new EmpleadoAsalariado(nombre, apellido, nss, salarioSemanal);
+                               
+                            }
+
+                            else if (existente is EmpleadoPorHoras)
+                            {
+                                Console.Write("Ingrese el nuevo sueldo por hora: ");
+                                sueldoPorHora = decimal.Parse(Console.ReadLine());
+
+                                Console.Write("Ingrese las nuevas horas trabajadas: ");
+                                horasTrabajadas = int.Parse(Console.ReadLine());
+
+                                empActualizado = new EmpleadoPorHoras(nombre, apellido, nss, sueldoPorHora, horasTrabajadas);
+                            }
+
+                            else if (existente is EmpleadoPorComision)
+                            {
+                                Console.Write("Ingrese las nuevas ventas brutas: ");
+                                ventasBrutas = decimal.Parse(Console.ReadLine());
+
+                                Console.Write("Ingrese la nueva tarifa de comisión: ");
+                                tarifaComision = int.Parse(Console.ReadLine());
+
+                                empActualizado = new EmpleadoPorComision(nombre, apellido, nss, ventasBrutas, tarifaComision);
+                                
+                            }
+
+                            else if (existente is EmpleadoAsalariadoPorComision)
+                            {
+                                Console.Write("Ingrese el nuevo salario semanal: ");
+                                salarioSemanal = decimal.Parse(Console.ReadLine());
+
+                                Console.Write("Ingrese las nuevas ventas brutas: ");
+                                ventasBrutas = decimal.Parse(Console.ReadLine());
+
+                                Console.Write("Ingrese la nueva tarifa de comisión (1-100): ");
+                                tarifaComision = int.Parse(Console.ReadLine());
+
+                                empActualizado = new EmpleadoAsalariadoPorComision(nombre, apellido, nss, salarioSemanal, ventasBrutas, tarifaComision);
+                            }
+
+                            // Actualizar en el servicio
+                            servicio.ActualizarEmpleado(empActualizado);
                         }
                         break;
 
@@ -112,6 +207,13 @@ namespace Sistema_Nominas.Presentacion
                         servicio.GenerarReporte();
                         break;
 
+                    case 5:
+                        Console.Clear();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Hasta pronto!");
+                        break;
                     default:
                         Console.WriteLine("La opción ingresada no existe");
                         break;
