35adaf8c35f8dbfd5c82275562c9b5a0b2e3535b
Gabriel Alcequiez <20251062@itla.edu.do>
2026-01-15 22:03:54 -0400

    Añadir la interfaz EmpleadoRepositorio y su implementacion

---
+++ Sistema_Nominas/Dominio/IEmpleadoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema_Nominas.Dominio
+{
+    public interface IEmpleadoRepositorio
+    {
+        void AgregarEmpleado(Empleado empleado);
+        void ActualizarEmpleado(Empleado empleado);
+
+        List<Empleado> listarEmpleados();
+        void GenerarReporte();
+    }
+}
---
+++ Sistema_Nominas/Infraestructura/EmpleadoRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using Sistema_Nominas.Dominio;
+
+namespace Sistema_Nominas.Infraestructura
+{
+    public class EmpleadoRepositorio : IEmpleadoRepositorio
+    {
+        private static List<Empleado> listaEmpleados = new List<Empleado>();
+
+        public void AgregarEmpleado(Empleado empleado)
+        {
+            listaEmpleados.Add(empleado);
+        }
+
+        public void ActualizarEmpleado(Empleado empleado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerarReporte()
+        {
+            foreach (var empleadoAUX in listaEmpleados)
+            {
+                decimal pago = empleadoAUX.CalcularPago();
+                Console.WriteLine($"{empleadoAUX.PrimerNombre} {empleadoAUX.ApellidoPaterno}, Tipo: {empleadoAUX.GetType().Name}. Pago: {pago:C}");
+            }
+        }
+
+        public List<Empleado> listarEmpleados()
+        {
+            return listaEmpleados;
+        }
+    }
+}
